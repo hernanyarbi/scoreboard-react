338555152da15f5053dddbb100e8b28917d99a72
Hernan Yarbi <hernan.yarbi@gmail.com>
2025-01-28 20:26:59 -0300

    feat: Updated some layout styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Scoreboard } from "./components/Scoreboard";
 
 function App() {
   return (
-    <div className="flex flex-col gap-6 bg-primary h-screen p-10 pt-16">
+    <div className="flex flex-col item items-center gap-6 h-screen p-10 pt-16 overflow-x-hidden">
       <h1 className="text-white text-3xl font-semibold">Resultado Fácil</h1>
-      <Scoreboard/>
+      <Scoreboard />
     </div>
   );
 }
